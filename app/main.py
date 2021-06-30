339b4953a15e9e5e3a990d460a4df57c3bcb6185
industArk <achoruzy@gmail.com>
2021-06-30 22:48:38 +0200

    started REST API code

--- app/main.py
+++ app/main.py
@@ -3,7 +3,10 @@
 #   Arkadiusz Choruzy
 #   achoruzy@gmail.com
 
+from typing import Optional
+
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 
 api = FastAPI()
@@ -11,4 +14,16 @@ api = FastAPI()
 
 @api.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "Kaszebe"}
+
+
+@api.get("/filter")
+async def filter_data(
+    school_name: Optional[str] = None,
+    category: Optional[str] = None,
+    pupils_more_than: Optional[int] = None,
+    pupils_less_than: Optional[int] = None,
+    female: Optional[bool] = True,
+    male: Optional[bool] = True,
+):
+    return {"school name": [school_name, category, pupils_more_than, female, male]}
