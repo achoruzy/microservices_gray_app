aa04378c8a42fee3519116af67bf6079c7f47278
industArk <achoruzy@gmail.com>
2021-08-07 23:44:37 +0200

    add first unit test for main.py api testing

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,3 +2,24 @@
 #
 #   Arkadiusz Choruzy
 #   achoruzy@gmail.com
+
+from fastapi.testclient import TestClient
+
+# -- INTERNAL IMPORTS --
+from app.main import api
+
+client = TestClient(api)
+
+
+def test_datarow():
+    response = client.get("/datarow?id=1")
+
+    json_resp = {total_enrollment: 162,
+                 id: 1,
+                 school_name: "P.S. 015 Roberto Clemente",
+                 male: 83,
+                 female: 79,
+                 category: "All Students"}
+
+    assert response.status_code == 200
+    assert response.json() == json_resp
