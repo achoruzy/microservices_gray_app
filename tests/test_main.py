c5098a3c61e69e75b06af59c008188a390f9effb
industArk <achoruzy@gmail.com>
2021-08-08 00:53:33 +0200

    add unit tests for main.py api functions

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,14 +4,19 @@
 #   achoruzy@gmail.com
 
 from fastapi.testclient import TestClient
+import pandas as pd
+
 
 # -- INTERNAL IMPORTS --
+from app import main
 from app.main import api
+from app.db import database
 
 client = TestClient(api)
 
 
 def test_datarow():
+    # Unit test is also responsible for testing main.all_data and database connection availability
     response = client.get("/datarow?id=1")
 
     json_resp = {'total_enrollment': 162,
@@ -23,3 +28,29 @@ def test_datarow():
 
     assert response.status_code == 200
     assert response.json() == json_resp
+
+
+def test_read_root():
+    response = client.get("/")
+
+    assert response.status_code == 200
+
+
+def test_filter():
+    response = client.get("filter?category=Poverty&more_than=650&gender=male")
+
+    assert response.status_code == 200
+
+
+def test_query_to_df():
+    with database.SessionLocal() as session:
+        data = main.query_to_df(session)
+
+        assert type(data) == pd.DataFrame
+
+
+def test_plot_chaart():
+    with database.SessionLocal() as session:
+        data = main.plot_chart(session)
+
+        assert type(data) == str
